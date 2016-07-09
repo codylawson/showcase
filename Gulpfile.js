16d06a53e460474e96908aca1cf4ec622b169e48
Cody Lawson <CLawson@esri.com>
2016-07-09 14:09:49 -0700

    Add sass and gulp-connect with livereload

---
+++ Gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    neat = require('node-neat').includePaths,
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    os = require('os');
+
+gulp.task('sass', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(sass({
+          includePaths: ['styles'].concat(neat)
+        }))
+        .pipe(gulp.dest('css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+var browser = os.platform() === 'linux' ? 'google-chrome' : (
+  os.platform() === 'darwin' ? 'google chrome' : (
+  os.platform() === 'win32' ? 'chrome' : 'firefox'));
+
+gulp.src('./package.json').pipe(open({app: 'chrome'}));
+
+gulp.task('open', function(){
+  gulp.src('./index.html')
+  .pipe(open({uri: 'http://localhost:8080'}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('./*.html', ['html'])
+});
+
+gulp.task('default', ['sass', 'connect', 'open', 'watch']);
